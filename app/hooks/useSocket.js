124ee5e70ca9318bebf2a87ba87fca23ecad1b0f
Sebastian Andreasson <xandse@MacBook-Pro-som-tillhor-Sebastian.local>
2021-11-03 14:16:08 +0100

    remove vite client, add nextjs

--- app/hooks/useSocket.js
+++ app/hooks/useSocket.js
@@ -1,6 +1,6 @@
-import { useEffect, useMemo } from 'react'
+import { useMemo } from 'react'
 import { io } from 'socket.io-client'
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = process.env.NEXT_PUBLIC_API_URL
 
 const useSocket = () => {
   const socket = useMemo(() => io(API_URL))
